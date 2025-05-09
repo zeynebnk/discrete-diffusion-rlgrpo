2342c75d5d7b1dfd40a1135ad7ce262b8b10f199
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-05-08 19:08:05 -0700

    Update rl_grpo.py

--- mask_policy/rl_grpo.py
+++ mask_policy/rl_grpo.py
@@ -7,11 +7,12 @@ import pickle
 
 from mask_policy import MaskPolicy
 
+
 class MaskPolicyDataset(Dataset):
     def __init__(self):
-        with open("logits_ds_X.pkl", "rb") as f:    
+        with open("logits_ds_X (1).pkl", "rb") as f:    
             self.X = pickle.load(f)
-        with open("logits_ds_y.pkl", "rb") as f:
+        with open("logits_ds_y (2).pkl", "rb") as f:
             self.y = pickle.load(f)
 
     def __len__(self):
@@ -22,8 +23,8 @@ class MaskPolicyDataset(Dataset):
 
 class DummyDataset(Dataset):
     def __init__(self):
-        self.X = torch.rand((100,5,2))
-        self.y = (self.X.sum(dim=2) > 1) * 1
+        self.X = torch.rand((100,5,256))
+        self.y = (self.X.sum(dim=2) > 128) * 1
         
     def __len__(self):
         return len(self.y)
@@ -41,23 +42,10 @@ def kl(p, p_ref):
     
     return kl.mean()
 
-def train_step():
-    # prep! 
-    G = 16  # num samples
-    epsilon = 0.2  # clip limit
-    beta = 0.0001  # kl penalty weight
-    lr = 0.001
-    
-    # cur and ref policy
-    hidden_size, n_layers, n_heads = 256, 64, 16
-    policy = MaskPolicy(2,hidden_size=hidden_size, n_layers=n_layers, n_heads=n_heads)
-    ref_policy = MaskPolicy(2,hidden_size=hidden_size, n_layers=n_layers, n_heads=n_heads).eval()
-    ref_policy.load_state_dict(policy.state_dict())
-    
-    optimizer = optim.Adam(policy.parameters(), lr=lr)
+def train_step(policy, ref_policy, optimizer, G, epsilon, beta):
 
     # pregen dataset, stepwise only!! 
-    dl = DataLoader(DummyDataset(), batch_size=128, shuffle=True)
+    dl = DataLoader(DummyDataset(), batch_size=16, shuffle=True)
     for k, data_batch in enumerate(dl):
         X, y = data_batch
         gen_len = X.shape[1]
@@ -73,7 +61,6 @@ def train_step():
             fwpass_ref = fwpass_ref.unsqueeze(1).expand(-1, G, -1)
 
             samples = torch.bernoulli(fwpass) # bs, G, gen_len
-
         
         fwpass_ref = fwpass_ref.clamp(1e-8, 1.0 - 1e-8)
            
@@ -104,34 +91,57 @@ def train_step():
         optimizer.zero_grad()
         total_loss.backward()
         
-        '''
-        for param in policy.parameters():
+        
+        '''for param in policy.parameters():
             if param.grad is not None:
                 print(param.grad.norm().item())
             else:
-                print("NONE")
-        '''
-        
-
+                print("NONE")'''
+    
         optimizer.step()
 
-        with torch.no_grad():
-            pred = (policy(X) > 0.5) * 1
-            print((pred == y).float().mean(dim=-1).mean())
-        
-        ref_policy.load_state_dict(policy.state_dict()) # for updating ref policy
 
         print(f"loss: {loss_policy.item()}, kl: {kl_penalty.item()}, avg_r: {rewards.mean().item()}, max_r: {rewards.max().item()}")
 
     return policy
 
 def train_loop():
-    all_grad_history = []
-    epochs = 100
+    # prep! 
+    update_ref_every = 10
+        
+    G = 4  # num samples
+    epsilon = 0.2  # clip limit
+    beta = 0.0001  # kl penalty weight
+    lr = 1e-5
 
+    # cur and ref policy
+    hidden_size, n_layers, n_heads = 256, 4, 8
+    policy = MaskPolicy(2,hidden_size=hidden_size, n_layers=n_layers, n_heads=n_heads)
+    ref_policy = MaskPolicy(2,hidden_size=hidden_size, n_layers=n_layers, n_heads=n_heads).eval()
+    ref_policy.load_state_dict(policy.state_dict())
+    
+    optimizer = optim.Adam(policy.parameters(), lr=lr)
+
+    epochs = 500
+
+    '''with open("logits_ds_X (1).pkl", "rb") as f:    
+        fullX = pickle.load(f)
+    with open("logits_ds_y (2).pkl", "rb") as f:
+        fully = pickle.load(f)'''
+
+    fullX = DummyDataset().X
+    fully = DummyDataset().y
     for i in range(epochs):
         print(f"Epoch {i+1} of {epochs}")
-        policy = train_step()
+        policy = train_step(policy, ref_policy, optimizer, G, epsilon, beta)
+
+        with torch.no_grad():
+            pred = (policy(fullX) > 0.5) * 1
+            print((pred == fully).float().mean(dim=-1).mean())
+
+        if i % update_ref_every == 0:
+            ref_policy.load_state_dict(policy.state_dict()) # for updating ref policy
+        
     
     torch.save(policy.state_dict(), "policy.pth")
 
