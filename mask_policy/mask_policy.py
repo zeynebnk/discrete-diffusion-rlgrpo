ee72ec43bded7ed5efba8c37a1475301c1fdf63d
zeynebnk <70820835+zeynebnk@users.noreply.github.com>
2025-05-23 22:49:04 -0700

    Update mask_policy.py

--- mask_policy/mask_policy.py
+++ mask_policy/mask_policy.py
@@ -1,6 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from typing import Tuple, Optional
+
 
 class MaskPolicy(nn.Module):
     def __init__(self, in_size=4096, hidden_size=256, n_layers=2, n_heads=4):
@@ -30,6 +32,54 @@ class MaskPolicy(nn.Module):
         X = self.transformer(X)  # bs, gen_len, hidden_size
         
         out = self.output_layer(X).squeeze(-1)  # bs, gen_len
+        
+        return out
+
+
+
+from modelling_llada import (
+    ModelConfig,
+    BufferCache,
+    RotaryEmbedding,
+    LLaDABlock,
+    _non_meta_init_device
+)
+
+class MaskPolicy_LLaDA(nn.Module):
+    def __init__(self, in_size=4096, hidden_size=256, n_layers=2, n_heads=4):
+        super().__init__()
+        
+        self.config = ModelConfig(
+            d_model=hidden_size,
+            n_heads=n_heads,
+            rope=True, 
+            rope_theta=10000.0,
+            max_sequence_length=2048
+        )
+        self.buffer_cache = BufferCache()
+        
+        self.proj = nn.Linear(in_size, hidden_size)
+        
+        # transformer 
+        self.blocks = nn.ModuleList([
+            LLaDABlock.build(i, self.config, self.buffer_cache)
+            for i in range(n_layers)
+        ])
+        
+        self.output_layer = nn.Sequential(
+            nn.Linear(hidden_size, hidden_size // 2),
+            nn.ReLU(),
+            nn.Linear(hidden_size // 2, 1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, inputs):
+        x = self.proj(inputs)  # bs, gen_len, hidden_size
+        
+        for block in self.blocks:
+            x, _ = block(x, attention_bias=None, layer_past=None, use_cache=False)
+        
+        out = self.output_layer(x).squeeze(-1)  # bs, gen_len
         
         return out
 
